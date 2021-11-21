08908f774e8a02d985c4f7b347194f5eea1306ad
Cindy Huang <cindyhuang425@gmail.com>
2021-11-21 14:55:42 -0500

    add single plant view

--- app/src/main/java/com/practice/planter/PlantsAdapter.kt
+++ app/src/main/java/com/practice/planter/PlantsAdapter.kt
@@ -1,5 +1,6 @@
 package com.practice.planter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,6 +8,7 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -25,6 +27,7 @@ class PlantsAdapter(private val plants: List<Plant>) : RecyclerView.Adapter<Plan
         val lastWatered : TextView = itemView.findViewById(R.id.plantCardLastWatered)
         val scheduleText : TextView = itemView.findViewById(R.id.plantCardSchedule)
         val button : Button = itemView.findViewById(R.id.plantCardButton)
+        val card : CardView = itemView.findViewById(R.id.plantCard)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -32,15 +35,27 @@ class PlantsAdapter(private val plants: List<Plant>) : RecyclerView.Adapter<Plan
         return MyViewHolder(view)    }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val context = holder.itemView.context
         val plant = plants[position]
         holder.plantName.text = plant.name
 
         var requestOptions = RequestOptions()
-        Glide.with(holder.itemView.context)
+        Glide.with(context)
             .load(plant.image)
             .apply( requestOptions.transform(CenterCrop() ,RoundedCorners(24)) )
             .into(holder.image)
 
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, SinglePlantActivity::class.java)
+                .apply {
+                    putExtra("name", plant.name)
+                    putExtra("image", plant.image)
+                    putExtra("schedule", plant.watering_time)
+                    putExtra("last_watered", plant.time_elapsed)
+                }
+            context.startActivity(intent)
+
+        }
 
     }
 
--- app/src/main/java/com/practice/planter/SinglePlantActivity.kt
+++ app/src/main/java/com/practice/planter/SinglePlantActivity.kt
@@ -1,11 +1,42 @@
 package com.practice.planter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class SinglePlantActivity : AppCompatActivity() {
+    private lateinit var imageSection : ConstraintLayout
+    private lateinit var image : ImageView
+    private lateinit var name : TextView
+    private lateinit var backButton : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_single_plant)
+
+        val plantName = intent.extras?.getString("name")
+        val imageURL = intent.extras?.getString("image")
+
+        imageSection = findViewById(R.id.plantImageSection)
+        image = imageSection.findViewById(R.id.plantFullImage)
+        name = imageSection.findViewById(R.id.singlePlantName)
+        Glide.with(applicationContext)
+            .load(imageURL)
+            .centerCrop()
+            .into(image)
+        name.text = plantName
+
+        backButton = findViewById(R.id.backIcon)
+        backButton.setOnClickListener {
+            val backIntent = Intent(this, MainActivity::class.java)
+            startActivity(backIntent)
+        }
     }
+
 }
